85f66a19fa68663e6079ccb429786d0d5820837a
Bhakti Nagarale <bhaktinagarale2001@gmail.com>
2024-11-11 17:55:00 +0530

    Add app.js

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
+require('dotenv').config();
 const express = require("express");
 const { google } = require("googleapis");
 const fs = require("fs");
 
 
 // Load Google Sheets credentials
+
 //const credentials = JSON.parse(fs.readFileSync("credentials/credentials.json"));
-const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+//const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+
+
+async function loadCredentials() {
+  try {
+    // Fetch the JSON from the public DigitalOcean Space URL
+    const response = await fetch('https://factorys.blr1.digitaloceanspaces.com/credentials.json');
+    const credentials = await response.json();
+
+    const auth = new google.auth.GoogleAuth({
+      credentials,
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+
+    return auth;
+  } catch (error) {
+    console.error("Error loading credentials:", error);
+    throw error;
+  }
+}
 
 const { client_email, private_key } = credentials;
 const scopes = ["https://www.googleapis.com/auth/spreadsheets"];
